a3b164d3ec2ad737fe18d2e0d4cdb83a3c95eb38
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2023-08-15 08:22:51 +0300

    email sender package

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:telegram/telegram.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,7 +15,22 @@ class _HomeScreenState extends State<HomeScreen> {
     return Column(
       children: [
         InkWell(
-          onHover: (v) {},
+          onHover: (v) {
+            Telegram.send(
+              username: '@youngphreatic',
+              message: 'Kudos bruv!',
+            );
+          },
+          onTap: () async {
+            final Email email = Email(
+              body: 'Email body',
+              subject: 'Email subject',
+              recipients: ['shadrack.sme@gmail.com'],
+              isHTML: false,
+            );
+
+            await FlutterEmailSender.send(email);
+          },
           child: CircleAvatar(
             backgroundColor: Colors.blue.shade300,
             minRadius: 80,
