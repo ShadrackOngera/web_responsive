78eb1105233752fdcb39619e88480e31b46150a7
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2023-08-15 08:23:57 +0300

    removed un used dependacies

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:telegram/telegram.dart';
-import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,22 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Column(
       children: [
         InkWell(
-          onHover: (v) {
-            Telegram.send(
-              username: '@youngphreatic',
-              message: 'Kudos bruv!',
-            );
-          },
-          onTap: () async {
-            final Email email = Email(
-              body: 'Email body',
-              subject: 'Email subject',
-              recipients: ['shadrack.sme@gmail.com'],
-              isHTML: false,
-            );
-
-            await FlutterEmailSender.send(email);
-          },
+          onHover: (v) {},
           child: CircleAvatar(
             backgroundColor: Colors.blue.shade300,
             minRadius: 80,
