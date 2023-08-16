56424a66b310b3fbaaafdcf6737b94472b60152e
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2023-08-16 14:11:47 +0300

    telegram sending message works

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:web_responsive/utilities/private_keys.dart';
@@ -16,26 +18,49 @@ class _HomeScreenState extends State<HomeScreen> {
 
     final response = await http.post(
       Uri.parse('https://api.telegram.org/bot$botToken/sendMessage'),
-      body: {
+      headers: {
+        'Content-Type': 'application/json', // Specify content type as JSON
+      },
+      body: json.encode({
         'chat_id': chatId,
         'text': 'Your message here',
-      },
+      }),
     );
 
     if (response.statusCode == 200) {
       print('Message sent successfully');
     } else {
       print('Failed to send message');
+      print('Response: ${response.body}');
     }
   }
+  // Future<void> sendMessageToUser() async {
+  //   final botToken = PrivateKeys.telegram_bot_token;
+  //   final chatId = PrivateKeys.chat_id;
+
+  //   final response = await http.post(
+  //     Uri.parse('https://api.telegram.org/bot$botToken/sendMessage'),
+  //     body: {
+  //       'chat_id': chatId,
+  //       'text': 'Your message here',
+  //     },
+  //   );
+
+  //   if (response.statusCode == 200) {
+  //     print('Message sent successfully');
+  //   } else {
+  //     print('Failed to send message');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        InkWell(
+        GestureDetector(
           onTap: () {
             sendMessageToUser();
+            print('object');
           },
           child: CircleAvatar(
             backgroundColor: Colors.blue.shade300,
